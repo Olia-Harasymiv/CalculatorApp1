447126778dde75bc1cebfaeed17ae046aebfe1c0
olia-harasymiv <olia.harasymiv@sombrainc.com>
2022-07-26 22:11:46 +0300

    Calc

--- src/Calculator.java
+++ src/Calculator.java
@@ -20,30 +20,23 @@ public class Calculator {
 
 
         switch (maths) {
-
-            case '+':
+            case '+' -> {
                 result = number1 + number2;
                 System.out.println(number1 + " + " + number2 + " = " + result);
-                break;
-
-            case '-':
+            }
+            case '-' -> {
                 result = number1 - number2;
                 System.out.println(number1 + " - " + number2 + " = " + result);
-                break;
-
-            case '*':
+            }
+            case '*' -> {
                 result = number1 * number2;
                 System.out.println(number1 + " * " + number2 + " = " + result);
-                break;
-
-            case '/':
+            }
+            case '/' -> {
                 result = number1 / number2;
                 System.out.println(number1 + " / " + number2 + " = " + result);
-                break;
-
-            default:
-                System.out.println("Invalid operator!");
-                break;
+            }
+            default -> System.out.println("Invalid operator!");
         }
 
         number.close();
